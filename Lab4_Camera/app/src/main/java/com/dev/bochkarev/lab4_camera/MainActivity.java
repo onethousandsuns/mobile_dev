6babf41d3d91731879a1973dc05bc1b75ade8d6a
onethousandsuns <qontrol1488@gmail.com>
2017-06-14 09:25:28 +0300

    Fixed: - all processes work in separate threads - added post photographing message

--- Lab4_Camera/app/src/main/java/com/dev/bochkarev/lab4_camera/MainActivity.java
+++ Lab4_Camera/app/src/main/java/com/dev/bochkarev/lab4_camera/MainActivity.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
@@ -27,15 +28,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageButton takePhotoButton = (ImageButton) findViewById(R.id.imageButton);
         _imageView = (ImageView) findViewById(R.id.imageView);
+
+        ImageButton takePhotoButton = (ImageButton) findViewById(R.id.imageButton);
         takePhotoButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        dispatchTakePictureIntent();
+                        new RunDispatchTakePictureIntent().execute();
                     }
                 });
+    }
+
+    private class RunDispatchTakePictureIntent extends AsyncTask<Void, Void, Intent> {
+        @Override
+        protected void onPreExecute(){
+            Toast.makeText(MainActivity.this,
+                    R.string.camera_running,
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected Intent doInBackground(Void... params) {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                return takePictureIntent;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Intent intent) {
+            // code below taken from https://developer.android.com/training/camera/photobasics.html
+            if (intent != null) {
+                File photoFile = null;
+                try {
+                    photoFile = new ImageLoader().execute((Void) null).get();
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+                if (photoFile != null) {
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                    startActivityForResult(intent, REQUEST_TAKE_PHOTO);
+                }
+            } else {
+                Toast.makeText(MainActivity.this,
+                        R.string.camera_error,
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
@@ -43,8 +84,14 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (resultCode == RESULT_OK && requestCode == REQUEST_TAKE_PHOTO) {
-            galleryAddPic();
-            setPic();
+            try{
+                new GalleryPictureAdder().execute();
+                new PictureViewer().execute();
+            } catch (Exception e){
+                Toast.makeText(MainActivity.this,
+                        R.string.camera_error,
+                        Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
@@ -53,70 +100,73 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected File doInBackground(Void... params) {
             try {
-                return getImage();
+                File storageDir = Environment.getExternalStorageDirectory();
+                File image = File.createTempFile("DCIM", ".jpg", storageDir);
+                _pathToPhoto = image.getAbsolutePath();
+                return image;
             } catch (IOException e) {
                 e.printStackTrace();
                 return null;
             }
         }
+    }
 
-        private File getImage() throws IOException {
-            File storageDir = Environment.getExternalStorageDirectory();
-            File image = File.createTempFile("DCIM", ".jpg", storageDir);
-            _pathToPhoto = image.getAbsolutePath();
-            return image;
+    private class GalleryPictureAdder extends AsyncTask<Void, Void, Void> {
+        Intent mediaScanIntent;
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            // code below taken from https://developer.android.com/training/camera/photobasics.html
+            mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            File f = new File(_pathToPhoto);
+            Uri contentUri = Uri.fromFile(f);
+            mediaScanIntent.setData(contentUri);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void param){
+            getApplicationContext().sendBroadcast(mediaScanIntent);
         }
     }
 
-
-    // code below taken from https://developer.android.com/training/camera/photobasics.html
-    private void galleryAddPic() {
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        File f = new File(_pathToPhoto);
-        Uri contentUri = Uri.fromFile(f);
-        mediaScanIntent.setData(contentUri);
-        this.sendBroadcast(mediaScanIntent);
-    }
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            File photoFile = null;
-            try {
-                photoFile = new ImageLoader().execute((Void) null).get();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            }
-
-            if (photoFile != null) {
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-            }
-        }
-    }
-
-    private void setPic() {
-        // Get the dimensions of the View
+    private class PictureViewer extends AsyncTask<Void, Void, Bitmap> {
         int targetW = _imageView.getWidth();
         int targetH = _imageView.getHeight();
 
-        // Get the dimensions of the bitmap
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        bmOptions.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(_pathToPhoto, bmOptions);
-        int photoW = bmOptions.outWidth;
-        int photoH = bmOptions.outHeight;
+        @Override
+        protected void onPreExecute(){
+            // Get the dimensions of the View
+            targetW = _imageView.getWidth();
+            targetH = _imageView.getHeight();
+        }
 
-        // Determine how much to scale down the image
-        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+        @Override
+        protected Bitmap doInBackground(Void... params) {
 
-        // Decode the image file into a Bitmap sized to fill the View
-        bmOptions.inJustDecodeBounds = false;
-        bmOptions.inSampleSize = scaleFactor;
+            // Get the dimensions of the bitmap
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            bmOptions.inJustDecodeBounds = true;
+            BitmapFactory.decodeFile(_pathToPhoto, bmOptions);
+            int photoW = bmOptions.outWidth;
+            int photoH = bmOptions.outHeight;
 
-        Bitmap bitmap = BitmapFactory.decodeFile(_pathToPhoto, bmOptions);
-        _imageView.setImageBitmap(bitmap);
+            // Determine how much to scale down the image
+            int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+
+            // Decode the image file into a Bitmap sized to fill the View
+            bmOptions.inJustDecodeBounds = false;
+            bmOptions.inSampleSize = scaleFactor;
+
+            return BitmapFactory.decodeFile(_pathToPhoto, bmOptions);
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap){
+            _imageView.setImageBitmap(bitmap);
+            Toast.makeText(MainActivity.this,
+                    getString(R.string.saved_to) + _pathToPhoto,
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
